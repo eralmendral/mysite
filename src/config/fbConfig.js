3b0bc915a48654e06177fc2a82c7c05f4530de53
eralmendral <eralmendral@gmail.com>
2020-08-20 23:34:28 +0800

    fix: undo eject + added env

--- src/config/fbConfig.js
+++ src/config/fbConfig.js
@@ -5,14 +5,14 @@ import "firebase/auth";
 import ReduxSagaFirebase from "redux-saga-firebase"
 
 export const firebaseConfig = {
-  apiKey: "AIzaSyBg_9g0fSqQ6asP68w81pr5GY1dosbSOsM",
-  authDomain: "mysite-a8cf4.firebaseapp.com",
-  databaseURL: "https://mysite-a8cf4.firebaseio.com",
-  projectId: "mysite-a8cf4",
-  storageBucket: "mysite-a8cf4.appspot.com",
-  messagingSenderId: "227209888066",
-  appId: "1:227209888066:web:76cece7df9b471df92c8db",
-  measurementId: "G-8B73XC7L04"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 export const firebaseApp = firebase.initializeApp(firebaseConfig)
