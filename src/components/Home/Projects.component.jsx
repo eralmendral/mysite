b3a4405c047e38605cc2bb04e537c3f69270c0d6
eralmendral <eralmendral@gmail.com>
2020-10-12 12:30:30 +0800

    fix: display clickable prod link

--- src/components/Home/Projects.component.jsx
+++ src/components/Home/Projects.component.jsx
@@ -18,7 +18,7 @@ const ProjectsComponent = ({ projects, history }) =>{
             <h3 className="text-center mt-5 sectionTitle">Projects</h3>
             <Container  className="projects-container">
             { Object.keys(projects).map((key, i) => (
-                <Card className='project-card' style={{ width: '18rem' }} onClick={(id) => showProject(projects[key].id)}>
+                <Card className='project-card' style={{ width: '20rem' }} onClick={(id) => showProject(projects[key].id)}>
                  <Card.Img className="img-bw" variant="top" src={projects[key].thumbnail} />
                  <Card.Body className="bg-dark text-white">
                      <Card.Title>{ projects[key].title }</Card.Title>
@@ -26,7 +26,7 @@ const ProjectsComponent = ({ projects, history }) =>{
                         { shortDescription(projects[key].description) }
                      </Card.Text>
                      { projects[key].github ? <Card.Link href={projects[key].github} target="_blank" onClick={(e) => e.stopPropagation()}><i className="fab fa-github fa-2x project-card-link" title="github repository" aria-hidden="true"></i></Card.Link> : ''}
-                     { projects[key].demo ? <Card.Link href={projects[key].demo} target="_blank" onClick={(e) => e.stopPropagation() }><i className="fas fa-link fa-2x project-card-link" title="url" aria-hidden="true"></i></Card.Link> : ''}
+                     { projects[key].demo || projects[key].prod ? <Card.Link href={projects[key].demo || projects[key].prod} target="_blank" onClick={(e) => e.stopPropagation() }><i className="fas fa-link fa-2x project-card-link" title="url" aria-hidden="true"></i></Card.Link> : ''}
                  </Card.Body>
              </Card>
             ))}
